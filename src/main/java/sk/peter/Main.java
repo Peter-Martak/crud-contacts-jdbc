8dd47a4fc01d4004c233bf3c8d133eb0a8f9aa1e
Peter <peter.martak1@gmail.com>
2025-08-12 17:45:38 +0200

    Connect to MySQL database and print all contacts

--- src/main/java/sk/peter/Main.java
+++ src/main/java/sk/peter/Main.java
@@ -1,7 +1,27 @@
 package sk.peter;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String select = "SELECT * FROM contact";
+        String connectionURL = "jdbc:mysql://localhost:3306/contacts";
+        
+        try {
+            Connection conn = DriverManager.getConnection(connectionURL, "root", "password");
+            PreparedStatement ps = conn.prepareStatement(select);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+
+                System.out.println("ID: " + id + ", name: " + name + ", email: " + email + ", phone: " + phone);
+
+            }
+        } catch (SQLException e){
+            System.out.println("Error while connecting to database");
+        }
     }
 }
