af85b0f532acb41d36e2a98ef229fa881781795e
Peter <peter.martak1@gmail.com>
2025-08-23 17:11:40 +0200

    Load DB connection values from properties

--- src/main/java/sk/peter/HikariCPDataSource.java
+++ src/main/java/sk/peter/HikariCPDataSource.java
@@ -2,18 +2,30 @@ package sk.peter;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class HikariCPDataSource {
     private static HikariConfig config = new HikariConfig();
     private static HikariDataSource ds;
 
+    private static final Logger logger = LoggerFactory.getLogger(HikariCPDataSource.class);
+
+
         static {
-            config.setJdbcUrl("jdbc:mysql://localhost:3306/contacts" );
-            config.setUsername("root");
-            config.setPassword("password");
+            final Properties prop = new Properties();
+            try {
+                prop.load(HikariCPDataSource.class.getResourceAsStream("/application.properties"));
+            } catch (Exception e){
+                logger.error("Error while loading application properties properties", e);
+            }
+            config.setJdbcUrl("jdbc:mysql://localhost:3306/" + prop.getProperty("db.name") );
+            config.setUsername(prop.getProperty("db.username"));
+            config.setPassword(prop.getProperty("db.password"));
             ds = new HikariDataSource( config );
         }
 
